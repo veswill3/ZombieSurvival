cd3d1b538c6fe7b2cc14bb424fa115374c65dc60
Vesper Williams <veswill3@gmail.com>
2012-11-01 20:55:20 -0500

    Zombies now follow player if close by. Also cleaned up a bunch of stuff

--- src/vesper/android/zombiesurvival/TestActivity.java
+++ src/vesper/android/zombiesurvival/TestActivity.java
@@ -1,9 +1,11 @@
 package vesper.android.zombiesurvival;
 
+import java.util.Random;
 import org.andengine.engine.camera.ZoomCamera;
 import org.andengine.engine.camera.hud.controls.AnalogOnScreenControl;
 import org.andengine.engine.camera.hud.controls.BaseOnScreenControl;
 import org.andengine.engine.camera.hud.controls.AnalogOnScreenControl.IAnalogOnScreenControlListener;
+import org.andengine.engine.handler.IUpdateHandler;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
@@ -19,8 +21,6 @@ import org.andengine.extension.physics.box2d.PhysicsConnector;
 import org.andengine.extension.physics.box2d.PhysicsFactory;
 import org.andengine.extension.physics.box2d.PhysicsWorld;
 import org.andengine.extension.physics.box2d.util.Vector2Pool;
-import org.andengine.input.sensor.acceleration.AccelerationData;
-import org.andengine.input.sensor.acceleration.IAccelerationListener;
 import org.andengine.input.touch.TouchEvent;
 import org.andengine.input.touch.detector.PinchZoomDetector;
 import org.andengine.input.touch.detector.PinchZoomDetector.IPinchZoomDetectorListener;
@@ -33,14 +33,13 @@ import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegion
 import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
-import android.hardware.SensorManager;
 import android.opengl.GLES20;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 
-public class TestActivity extends SimpleBaseGameActivity implements IAccelerationListener,
+public class TestActivity extends SimpleBaseGameActivity implements //IAccelerationListener,
 																	IOnSceneTouchListener,
 																	IOnAreaTouchListener,
 																	IScrollDetectorListener,
@@ -49,8 +48,10 @@ public class TestActivity extends SimpleBaseGameActivity implements IAcceleratio
 	// Constants
 	// ===========================================================
 
-	private static final int CAMERA_WIDTH = 800;
-	private static final int CAMERA_HEIGHT = 480;
+	private static final int LEVEL_WIDTH = 2000;
+	private static final int LEVEL_HEIGHT = 1000;
+	private static final int DEFAULT_CAMERA_WIDTH = 800;
+	private static final int DEFAULT_CAMERA_HEIGHT = 480;
 
 	// ===========================================================
 	// Fields
@@ -77,8 +78,8 @@ public class TestActivity extends SimpleBaseGameActivity implements IAcceleratio
 	// physics related
 	private PhysicsWorld mPhysicsWorld;
 
-	private float mGravityX;
-	private float mGravityY;
+	//private float mGravityX;
+	//private float mGravityY;
 
 	private Scene mScene;
 
@@ -96,9 +97,9 @@ public class TestActivity extends SimpleBaseGameActivity implements IAcceleratio
 
 	@Override
 	public EngineOptions onCreateEngineOptions() {
-		this.mZoomCamera = new ZoomCamera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		this.mZoomCamera = new ZoomCamera(0, 0, DEFAULT_CAMERA_WIDTH, DEFAULT_CAMERA_HEIGHT);
 		return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED,
-				new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mZoomCamera);
+				new RatioResolutionPolicy(DEFAULT_CAMERA_WIDTH, DEFAULT_CAMERA_HEIGHT), mZoomCamera);
 	}
 
 	@Override
@@ -126,7 +127,8 @@ public class TestActivity extends SimpleBaseGameActivity implements IAcceleratio
 	public Scene onCreateScene() {
 		this.mEngine.registerUpdateHandler(new FPSLogger());
 
-		this.mPhysicsWorld = new PhysicsWorld(new Vector2(0, SensorManager.GRAVITY_EARTH), false);
+		// this.mPhysicsWorld = new PhysicsWorld(new Vector2(0, SensorManager.GRAVITY_EARTH), false);
+		this.mPhysicsWorld = new PhysicsWorld(new Vector2(0, 0), false);
 
 		Scene scene = new Scene();
 		this.mScene = scene;
@@ -145,6 +147,13 @@ public class TestActivity extends SimpleBaseGameActivity implements IAcceleratio
 		createBorders(scene, vertexBufferObjectManager);
 		createTestBarrier(scene, vertexBufferObjectManager);
 		createPlayer(scene, vertexBufferObjectManager);
+
+		// add some zombies randomly
+		Random rand = new Random();
+		for (int i = 0; i < 50; i++) {
+			addZombie(rand.nextInt(LEVEL_WIDTH - 64) + 32, rand.nextInt(LEVEL_HEIGHT - 64) + 32);
+		}
+		
 		initOnScreenControls(scene, vertexBufferObjectManager);
 		
 		return scene;
@@ -153,7 +162,7 @@ public class TestActivity extends SimpleBaseGameActivity implements IAcceleratio
 	private void createPlayer(Scene scene, VertexBufferObjectManager vertexBufferObjectManager) {
 		// create android in middle of screen
 		final FixtureDef objectFixtureDef = PhysicsFactory.createFixtureDef(1, 0.5f, 0.5f);
-		final Sprite androidSprite = new Sprite(CAMERA_WIDTH / 2, CAMERA_HEIGHT / 2,
+		final Sprite androidSprite = new Sprite(LEVEL_WIDTH / 2, LEVEL_HEIGHT / 2,
 				this.mAndroidTextureRegion, vertexBufferObjectManager);
 		mAndroid = androidSprite;
 		final Body body = PhysicsFactory.createCircleBody(this.mPhysicsWorld, androidSprite,
@@ -167,10 +176,10 @@ public class TestActivity extends SimpleBaseGameActivity implements IAcceleratio
 	private void createBorders(Scene scene, final VertexBufferObjectManager vertexBufferObjectManager) {
 		final FixtureDef wallFixtureDef = PhysicsFactory.createFixtureDef(0, 0.5f, 0.5f);
 		
-		final Rectangle ground = new Rectangle(0, CAMERA_HEIGHT - 2, CAMERA_WIDTH, 2, vertexBufferObjectManager);
-		final Rectangle roof = new Rectangle(0, 0, CAMERA_WIDTH, 2, vertexBufferObjectManager);
-		final Rectangle left = new Rectangle(0, 0, 2 , CAMERA_HEIGHT, vertexBufferObjectManager);
-		final Rectangle right = new Rectangle(CAMERA_WIDTH - 2, 0, 2, CAMERA_HEIGHT, vertexBufferObjectManager);
+		final Rectangle ground = new Rectangle(0, LEVEL_HEIGHT - 2, LEVEL_WIDTH, 2, vertexBufferObjectManager);
+		final Rectangle roof = new Rectangle(0, 0, LEVEL_WIDTH, 2, vertexBufferObjectManager);
+		final Rectangle left = new Rectangle(0, 0, 2 , LEVEL_HEIGHT, vertexBufferObjectManager);
+		final Rectangle right = new Rectangle(LEVEL_WIDTH - 2, 0, 2, LEVEL_HEIGHT, vertexBufferObjectManager);
 		
 		PhysicsFactory.createBoxBody(this.mPhysicsWorld, ground, BodyType.StaticBody, wallFixtureDef);
 		PhysicsFactory.createBoxBody(this.mPhysicsWorld, roof, BodyType.StaticBody, wallFixtureDef);
@@ -186,22 +195,20 @@ public class TestActivity extends SimpleBaseGameActivity implements IAcceleratio
 	private void createTestBarrier(Scene scene, final VertexBufferObjectManager vertexBufferObjectManager) {
 		final FixtureDef wallFixtureDef = PhysicsFactory.createFixtureDef(0, 0.5f, 0.5f);
 		
-		/*
-		 *                  _______
+		/*                   _____
 		 *                  |     |
-		 *    ______________|_lenB|
+		 *     _____________|_lenB|
 		 *    |                   |hB
 		 *  hA|________lenA_______|
-		 * 
 		 */
 		
-		int lenA = CAMERA_WIDTH / 2;
-		int lenB = CAMERA_WIDTH / 8;
-		int hA = CAMERA_HEIGHT / 5;
-		int hB = CAMERA_HEIGHT / 3;
+		int lenA = LEVEL_WIDTH / 2;
+		int lenB = LEVEL_WIDTH / 8;
+		int hA = LEVEL_HEIGHT / 5;
+		int hB = LEVEL_HEIGHT / 3;
 		
-		int xA = CAMERA_WIDTH / 6;
-		int yA = CAMERA_HEIGHT / 6;
+		int xA = LEVEL_WIDTH / 6;
+		int yA = LEVEL_HEIGHT / 6;
 		int xB = xA + lenA - lenB;
 		int yB = yA; // so they overlap. Otherwise bodies would get stuck in the crack
 		
@@ -217,23 +224,20 @@ public class TestActivity extends SimpleBaseGameActivity implements IAcceleratio
 
 	private void initOnScreenControls(Scene scene, final VertexBufferObjectManager vertexBufferObjectManager) {
 		final AnalogOnScreenControl analogOnScreenControl =
-				new AnalogOnScreenControl(0, CAMERA_HEIGHT - this.mOnScreenControlBaseTextureRegion.getHeight(),
+				new AnalogOnScreenControl(0, DEFAULT_CAMERA_HEIGHT - this.mOnScreenControlBaseTextureRegion.getHeight(),
 						this.mZoomCamera, this.mOnScreenControlBaseTextureRegion, this.mOnScreenControlKnobTextureRegion,
 						0.1f, vertexBufferObjectManager, new IAnalogOnScreenControlListener() {
 			@Override
 			public void onControlChange(final BaseOnScreenControl pBaseOnScreenControl,
 					final float pValueX, final float pValueY) {
-				final Body carBody = (Body)TestActivity.this.mAndroid.getUserData();
-				final Vector2 velocity = Vector2Pool.obtain(pValueX * 200, pValueY * 200);
-				if (velocity.len2() > 1) {
-					carBody.setLinearVelocity(velocity);
-				}
+				final Body androidBody = (Body)TestActivity.this.mAndroid.getUserData();
+				final Vector2 velocity = Vector2Pool.obtain(pValueX * 20, pValueY * 20);
+				androidBody.setLinearVelocity(velocity);
 				Vector2Pool.recycle(velocity);
 
-//				final float rotationInRad = (float)Math.atan2(-pValueX, pValueY);
-//				carBody.setTransform(carBody.getWorldCenter(), rotationInRad);
-
-//				TestActivity.this.mAndroid.setRotation(MathUtils.radToDeg(rotationInRad));
+				//final float rotationInRad = (float)Math.atan2(-pValueX, pValueY);
+				//androidBody.setTransform(androidBody.getWorldCenter(), rotationInRad);
+				//TestActivity.this.mAndroid.setRotation(MathUtils.radToDeg(rotationInRad));
 			}
 
 			@Override
@@ -243,9 +247,6 @@ public class TestActivity extends SimpleBaseGameActivity implements IAcceleratio
 		});
 		analogOnScreenControl.getControlBase().setBlendFunction(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
 		analogOnScreenControl.getControlBase().setAlpha(0.5f);
-		//		analogOnScreenControl.getControlBase().setScaleCenter(0, 128);
-		//		analogOnScreenControl.getControlBase().setScale(0.75f);
-		//		analogOnScreenControl.getControlKnob().setScale(0.75f);
 		analogOnScreenControl.refreshControlKnobPosition();
 
 		scene.setChildScene(analogOnScreenControl);
@@ -277,11 +278,6 @@ public class TestActivity extends SimpleBaseGameActivity implements IAcceleratio
 		} else {
 			if(pSceneTouchEvent.isActionDown()) {
 				this.mScrollDetector.setEnabled(true);
-				if(this.mPhysicsWorld != null) {
-					if(pSceneTouchEvent.isActionDown()) {
-						this.addZombie(pSceneTouchEvent.getX(), pSceneTouchEvent.getY());
-					}
-				}
 			}
 			this.mScrollDetector.onTouchEvent(pSceneTouchEvent);
 		}
@@ -290,25 +286,10 @@ public class TestActivity extends SimpleBaseGameActivity implements IAcceleratio
 	}
 
 	@Override
-	public void onAccelerationAccuracyChanged(final AccelerationData pAccelerationData) {
-
-	}
-
-	@Override
-	public void onAccelerationChanged(final AccelerationData pAccelerationData) {
-		this.mGravityX = pAccelerationData.getX();
-		this.mGravityY = pAccelerationData.getY();
-
-		final Vector2 gravity = Vector2Pool.obtain(this.mGravityX, this.mGravityY);
-		this.mPhysicsWorld.setGravity(gravity);
-		Vector2Pool.recycle(gravity);
-	}
-
-	@Override
 	public void onResumeGame() {
 		super.onResumeGame();
 
-		this.enableAccelerationSensor(this);
+		//this.enableAccelerationSensor(this);
 	}
 
 	@Override
@@ -333,6 +314,25 @@ public class TestActivity extends SimpleBaseGameActivity implements IAcceleratio
 
 		this.mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(zombieSprite, zombieBody, true, true));
 
+		zombieSprite.registerUpdateHandler(new IUpdateHandler() {
+			
+			@Override
+			public void reset() {
+			}
+			
+			@Override
+			public void onUpdate(float pSecondsElapsed) {
+				// follow player
+				final Vector2 distance = Vector2Pool.obtain(mAndroid.getX(), mAndroid.getY());
+				distance.sub(zombieSprite.getX(), zombieSprite.getY());
+				// only if close enough to smell him
+				if (distance.len() < 50) {
+					zombieBody.setLinearVelocity(distance.nor().mul(10));
+				}
+				Vector2Pool.recycle(distance);
+			}
+		});
+		
 		zombieSprite.setUserData(zombieBody);
 		this.mScene.registerTouchArea(zombieSprite);
 		this.mScene.attachChild(zombieSprite);
@@ -340,9 +340,11 @@ public class TestActivity extends SimpleBaseGameActivity implements IAcceleratio
 
 	private void jumpZombie(final Sprite zombie) {
 		final Body zombieBody = (Body)zombie.getUserData();
-
-		final Vector2 velocity = Vector2Pool.obtain(this.mGravityX * -50, this.mGravityY * -50);
-		zombieBody.setLinearVelocity(velocity);
+		Random rand = new Random();
+		final float x = rand.nextFloat();
+		final float y = rand.nextFloat();
+		final Vector2 velocity = Vector2Pool.obtain(x, y);
+		zombieBody.setLinearVelocity(velocity.nor().mul(200));
 		Vector2Pool.recycle(velocity);
 	}
 
