2477470d9b28886c4caf4ac3d053d21fff0c5fb7
Vesper Williams <veswill3@gmail.com>
2012-11-15 23:15:16 -0600

    removed the touchhandler from zombie that made them jump

--- src/vesper/android/zombiesurvival/Zombie.java
+++ src/vesper/android/zombiesurvival/Zombie.java
@@ -1,13 +1,11 @@
 package vesper.android.zombiesurvival;
 
-import java.util.Random;
 import org.andengine.engine.handler.IUpdateHandler;
 import org.andengine.entity.Entity;
 import org.andengine.extension.physics.box2d.PhysicsFactory;
 import org.andengine.extension.physics.box2d.PhysicsWorld;
 import org.andengine.extension.physics.box2d.util.Vector2Pool;
 import org.andengine.extension.physics.box2d.util.constants.PhysicsConstants;
-import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 import com.badlogic.gdx.math.Vector2;
@@ -55,20 +53,6 @@ public class Zombie extends PhysicalSprite {
 			getBody().setLinearVelocity(toTarget.nor().mul(10));
 		}
 		Vector2Pool.recycle(toTarget);
-	}
-
-	@Override
-	public boolean onAreaTouched(TouchEvent pSceneTouchEvent,
-			float pTouchAreaLocalX, float pTouchAreaLocalY) {
-		if(pSceneTouchEvent.isActionDown()) {
-			Random rand = new Random();
-			final float x = rand.nextFloat();
-			final float y = rand.nextFloat();
-			final Vector2 velocity = Vector2Pool.obtain(x, y);
-			getBody().setLinearVelocity(velocity.nor().mul(200));
-			Vector2Pool.recycle(velocity);
-		}
-		return true;
 	}
 
 	public Zombie set(float x, float y) {
